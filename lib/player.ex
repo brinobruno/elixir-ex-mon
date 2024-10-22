d469a1b97a1cf9b529c56a761ac53f13e50f6b88
Bruno Correa <brunosantos6ft@gmail.com>
2024-10-21 22:28:17 -0300

    chore: Module improvements

--- lib/player.ex
+++ lib/player.ex
@@ -1,15 +1,17 @@
 defmodule ExMon.Player do
-  @enforce_keys [:name, :life, :move_rnd, :move_avg, :move_heal]
+  @required_keys [:life, :move_avg, :move_heal, :move_rnd, :name]
+  @max_life 100
 
-  defstruct [:name, :life, :move_rnd, :move_avg, :move_heal]
+  @enforce_keys @required_keys
+  defstruct @required_keys
 
   def build(name, move_avg, move_rnd, move_heal) do
     %ExMon.Player{
-      name: name,
+      life: @max_life,
       move_avg: move_avg,
-      move_rnd: move_rnd,
       move_heal: move_heal,
-      life: 100
+      move_rnd: move_rnd,
+      name: name
     }
   end
 end
