85e71a3d8f36bae9f4e3bced3db68bdec445be2e
Bruno Correa <brunosantos6ft@gmail.com>
2024-10-22 19:14:27 -0300

    feat: Use agents, add start_game and round info

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -1,7 +1,18 @@
 defmodule ExMon do
-  alias ExMon.Player
+  alias ExMon.{Game, Player} # To avoid ExMon.<other_module_names>
+  alias ExMon.Game.Status
+
+  @computer_name "Robotinik"
 
   def create_player(name, move_avg, move_heal, move_rnd) do
     Player.build(name, move_avg, move_heal, move_rnd)
+  end
+
+  def start_game(player) do
+    @computer_name
+    |> create_player(:punch, :kick, :heal)
+    |> Game.start(player)
+
+    Status.print_round_message()
   end
 end
