b96f8fd7b153026e93dd725de9c5ed056917ed2d
Bruno Correa <brunosantos6ft@gmail.com>
2024-10-23 16:10:00 -0300

    feat: Update opponent health

--- lib/game.ex
+++ lib/game.ex
@@ -16,9 +16,13 @@ defmodule ExMon.Game do
     Agent.get(__MODULE__, & &1)
   end
 
+
+  def update(state) do
+    # Since override with new value, don't need previous state
+    Agent.update(__MODULE__, fn _ -> state end)
+  end
+
   def player, do: Map.get(info(), :player)
-
   def turn, do: Map.get(info(), :turn)
-
   def fetch_player(player), do: Map.get(info(), player)
 end
