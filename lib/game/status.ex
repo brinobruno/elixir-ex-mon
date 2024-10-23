a8f6ac9c22765d4da8a933e08165835add6c7152
Bruno Correa <brunosantos6ft@gmail.com>
2024-10-23 16:47:29 -0300

    feat: Update game status

--- lib/game/status.ex
+++ lib/game/status.ex
@@ -1,9 +1,19 @@
 defmodule ExMon.Game.Status do
-  alias ExMon.Game
-
-  def print_round_message() do
+  def print_round_message(%{status: :started} = info) do
     IO.puts("\n===== The game has started =====\n")
-    IO.inspect(Game.info())
+    IO.inspect(info)
+    IO.puts("\n------------------------------")
+  end
+
+  def print_round_message(%{status: :continue, turn: player} = info) do
+    IO.puts("\n===== It's #{player} turn =====\n")
+    IO.inspect(info)
+    IO.puts("\n------------------------------")
+  end
+
+  def print_round_message(%{status: :game_over} = info) do
+    IO.puts("\n===== The game is over =====\n")
+    IO.inspect(info)
     IO.puts("\n------------------------------")
   end
 
@@ -12,10 +22,10 @@ defmodule ExMon.Game.Status do
   end
 
   def print_move_message(:computer, :attack, damage) do
-    IO.puts("\n===== The player attacked the computer dealing #{damage}. =====\n")
+    IO.puts("\n===== The player attacked the computer dealing #{damage} damage. =====\n")
   end
 
   def print_move_message(:player, :attack, damage) do
-    IO.puts("\n===== The computer attacked the player dealing: #{damage}. =====\n")
+    IO.puts("\n===== The computer attacked the player dealing: #{damage} damage. =====\n")
   end
 end
